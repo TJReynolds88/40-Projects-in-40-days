707da6ecf742c82a92fb0ad9c535daa26f541ddc
TJ Reynolds <tj.reynolds1988@gmail.com>
2020-11-26 19:42:43 -0600

    Add files via upload
    Day 1, Dad Joke API

---
+++ JokeAPI/script.js
@@ -0,0 +1,21 @@
+const jokeEl = document.getElementById('joke');
+const get_joke = document.getElementById('get_joke');
+
+get_joke.addEventListener('click', generateJoke );
+
+generateJoke();
+
+async function generateJoke() {
+    // call the API
+    const jokeRes = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    const joke = await jokeRes.json();
+
+    jokeEl.innerHTML = joke.joke;
+    // set the new joke
+
+}
